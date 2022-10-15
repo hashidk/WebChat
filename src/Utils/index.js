24c6903e4cbec4d6dc59625b5ad2edf1775559ac
Willian Medina <wandres_mv1@hotmail.com>
2022-10-15 11:34:23 -0500

    Update index.js

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,10 +1,10 @@
 
 const allow_cors = [
-    "https://chatweb-wm.herokuapp.com", 
-    'http://chatweb-wm.herokuapp.com', 
-    'https://chatweb-wm.herokuapp.com:21988', 
-    'https://chatweb-wm.herokuapp.com:80', 
-    'https://chatweb-wm.herokuapp.com:443'
+    "https://chatweb-wm.herokuapp.com/", 
+    'http://chatweb-wm.herokuapp.com/', 
+    'https://chatweb-wm.herokuapp.com:21988/', 
+    'https://chatweb-wm.herokuapp.com:80/', 
+    'https://chatweb-wm.herokuapp.com:443/'
 ]
 
 module.exports = {
